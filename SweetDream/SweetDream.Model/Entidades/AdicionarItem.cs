dc2ec988c113d88ada7f71ce1d6f2fffbf9e4ba9
Nicholas Giudice <nicholas@ciandt.com>
2017-11-21 21:44:03 -0200

    Gerenciar Carrinho

---
+++ SweetDream/SweetDream.Model/Entidades/AdicionarItem.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SweetDream.Model.Entidades
+{
+    public class AdicionarItem
+    {
+        public string idCliente { get; set; }
+        public string idProduto { get; set; }
+        public int quantidade { get; set; }
+    }
+}
